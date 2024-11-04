f712220b2540db14330a2ccbc8d2588cfd80b53d
max <uscpa@karathome.com>
2024-11-04 14:54:07 -0600

    Revert "finish phone review section with animation"
    This reverts commit 8585a887805848f721d4a5fb45506bf0bdf0e479.

--- src/app/components/reviews.tsx
+++ src/app/components/reviews.tsx
@@ -4,8 +4,7 @@ import { HTMLAttributes, useEffect, useRef, useState } from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { useInView } from 'framer-motion'
 import { cn } from '@/lib/utils'
-// need to delete .next folder to work
-import Phone from './Phone.tsx'
+import Phone from './Phone'
 
 const PHOPIC = [
   '/testimonials/1.jpg',
