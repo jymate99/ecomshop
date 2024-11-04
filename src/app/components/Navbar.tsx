fcfa50917180cab45e763074e88284c676b60339
max <uscpa@karathome.com>
2024-11-04 16:12:37 -0600

    finish the home page here

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { buttonVariants } from "../../../components/ui/button"
+import { buttonVariants } from "@/components/ui/button"
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import Link from "next/link"
 import { ArrowRight } from "lucide-react"
