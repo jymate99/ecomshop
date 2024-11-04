24280de32a8b09335743a60e95c8af5547f5e4a5
max <uscpa@karathome.com>
2024-11-04 15:33:15 -0600

    first commit after revert, it works on local

--- src/app/components/reviews.tsx
+++ src/app/components/reviews.tsx
@@ -4,7 +4,7 @@ import { HTMLAttributes, useEffect, useRef, useState } from 'react'
 import MaxWidthWrapper from './MaxWidthWrapper'
 import { useInView } from 'framer-motion'
 import { cn } from '@/lib/utils'
-import Phone from './Phone'
+import Phone from './phone'
 
 const PHONES = [
   '/testimonials/1.jpg',
